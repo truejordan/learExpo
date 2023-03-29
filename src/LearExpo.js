a6928bb09efc4b16b90dac434dfe9f9dfe7d5edd
truejordan <jordan.atlanta.henry@gmail.com>
2023-03-29 18:40:07 +0100

    1st commit

---
+++ src/LearExpo.js
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native"; 
+
+
+const LearExpo = () => {
+	return (
+		<View style={''}>
+			<Text>yooo some stuff will go here</Text>
+		</View>
+	);
+};
+export default LearExpo;
